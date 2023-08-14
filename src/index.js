ce8d3fcc49d19e711e52175c76a19725446a5938
tt-max14 <137813252+tt-max14@users.noreply.github.com>
2023-08-14 18:37:58 +0300

    feat: well done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = str.length;
+  bracketsConfig.forEach(a => str = str.split(a[0] + a[1]).join(''));
+  return str.length === 0 ? true : (str.length === bracket ? false : check(str, bracketsConfig));
 }
